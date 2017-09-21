2332801285229ab0b69919662b44accc9583c1da
Unknown <up687791@myport.ac.uk>
2017-09-21 15:45:50 +0200

    fix deployment script

--- install.sh
+++ install.sh
@@ -1,17 +1,13 @@
-
-
-sudo apt-get install apache2
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
-sudo apt-get update
+SYMFONY_DEBUG=0
+SYMFONY_ENV=prod
 git clone https://github.com/upMKuhn/trivagoC2.git
 cd trivagoC2
-
-php bin/symfony_requirements
-composer install --no-dev --optimize-autoloader
-php bin/console doctrine:migrations:execute 1
+composer install --no-dev --optimize-autoloader -n
+php bin/symfony_requirements --env=prod --no-debug
+php bin/console doctrine:migrations:execute 1 --env=prod --no-debug
 php bin/console cache:clear --env=prod --no-debug --no-warmup
-php bin/console cache:warmup --env=prod
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/example.com.conf
+php bin/console cache:warmup --env=prod --no-debug
+
  
 
 
